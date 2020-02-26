805aea8eebde6514f164dd03c0eed477437442ca
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-02-26 18:18:23 -0500

    merging back-End with Front-End, added fetch to signin

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express(); //we run express
 const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 const database = {
 	users: [
 	{
 		id: '123',
 		name: 'John',
+		password: 'cookies',
 		email: 'john@gmail.com',
 		password: 'cookies',
 		entries: 0, //track score
@@ -17,6 +20,7 @@ const database = {
 	{
 		id: '124',
 		name: 'Ryan',
+		password: 'Lion',
 		email: 'ryansmith@gmail.com',
 		password: 'banana',
 		entries: 0, //track score
@@ -31,6 +35,13 @@ app.get('/', (req, res) => { //in root
 
 // start with the signin endpoint
 app.post('/signin', (req,res) => {
+	// 	// Load hash from your password DB.
+	// bcrypt.compare("bacon", hash, function(err, res) {
+	//     // res == true
+	// });
+	// bcrypt.compare("veggies", hash, function(err, res) {
+	//     // res = false
+	// });
 	//compare value given in POSTMAN with the value hardcoded in our "database".
 	if(req.body.email == database.users[0].email && 
 		req.body.password == database.users[0].password) {
@@ -89,14 +100,6 @@ app.put('/image', (req,res) => {
 	}
 })
 
-
-// Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
 
 app.listen(3000, () =>{
 	console.log('app is running on port 3000');// message after port 3000 is run
