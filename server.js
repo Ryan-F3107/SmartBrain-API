0b1b195e4d3cc6bb260e6ecac7b6b65583bacfc7
Ryan-F3107 <47283124+Ryan-F3107@users.noreply.github.com>
2020-02-29 20:45:32 -0500

    added some changes

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/signin', (req,res) => {
 	//compare value given in POSTMAN with the value hardcoded in our "database".
 	if(req.body.email == database.users[0].email && 
 		req.body.password == database.users[0].password) {
-		res.json("success");
+		res.json(database.users[0]);
 	}
 	else{
 		res.status(400).json('error logging in');
@@ -63,7 +63,6 @@ app.post('/register', (req,res) => {
 		id: '125',
 		name: name,
 		email: email,
-		password: password,
 		entries: 0, //track score
 		joined: new Date() 
 	})
